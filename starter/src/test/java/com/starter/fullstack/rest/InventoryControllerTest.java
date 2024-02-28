ff0f8aca97bab49767992015b7b279d283a62821
petersm8 <42871805+petersm8@users.noreply.github.com>
2024-02-28 00:27:35 -0500

    create teste passes now

--- starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
+++ starter/src/test/java/com/starter/fullstack/rest/InventoryControllerTest.java
@@ -1,6 +1,5 @@
 package com.starter.fullstack.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.starter.fullstack.api.Inventory;
 import com.starter.fullstack.dao.InventoryDAO;
 import org.junit.After;
@@ -12,11 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,26 +19,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class InventoryControllerTest {
 
   @Autowired
-  private MockMvc mockMvc;
-
-  @Autowired
   private MongoTemplate mongoTemplate;
-  
-  @Autowired
-  private ObjectMapper objectMapper;
 
-  @Autowired
   private InventoryDAO inventoryDAO;
+
+  private InventoryController inventoryController;
 
   private Inventory inventory;
 
   @Before
   public void setup() throws Throwable {
     this.inventory = new Inventory();
-    this.inventory.setId("1");
-    this.inventory.setName("Item 1");
+    this.inventory.setId("ID");
+    this.inventory.setName("TEST");
     this.inventoryDAO = new InventoryDAO(this.mongoTemplate);
-    this.mongoTemplate.save(this.inventory);
+    this.inventory = this.mongoTemplate.save(this.inventory);
+    this.inventoryController = new InventoryController(this.inventoryDAO);
   }
 
   @After
@@ -52,16 +43,11 @@ public class InventoryControllerTest {
   }
 
   @Test
-  public void testCreate() throws Throwable {
+  public void create() throws Throwable {
     this.inventory = new Inventory();
-    this.inventory.setId("1");
-    this.inventory.setName("Item 1");
-    this.inventoryDAO.create(this.inventory);
-    this.mockMvc.perform(post("/inventory")
-        .accept(MediaType.APPLICATION_JSON)
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(this.objectMapper.writeValueAsString(this.inventory)))
-      .andExpect(status().isOk());
+    this.inventory.setId("OTHER ID");
+    this.inventory.setName("ALSO TEST");
+    this.inventoryController.create(this.inventory);
 
     Assert.assertEquals(2, this.mongoTemplate.findAll(Inventory.class).size());
   }
